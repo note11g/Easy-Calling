95c741a40019ecc78c26f40e79c24f9886832f92
skyneton <donginbae30@gmail.com>
2020-11-22 13:36:55 +0900

    GetPhoneItems Update

--- app/src/main/java/com/note11/easy_calling/screen/GetPhonesActivity.java
+++ app/src/main/java/com/note11/easy_calling/screen/GetPhonesActivity.java
@@ -1,5 +1,6 @@
 package com.note11.easy_calling.screen;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Bundle;
 
@@ -8,31 +9,90 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.note11.easy_calling.R;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class GetPhonesActivity extends AppCompatActivity {
+
+    HashMap<String, String> phoneList;
+
+    Button searchBtn;
+    EditText searchInp;
+    RecyclerView getPhonesRv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_phones);
 
-        getPhoneList();
+        phoneList = getPhoneList();
+
+        init();
+
+        drawPhoneItems(phoneList);
     }
 
-    public void getPhoneList() {
-        Cursor contacts = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+    protected void init() {
+        searchBtn = findViewById(R.id.searchBtn);
+        searchInp = findViewById(R.id.searchInp);
+        getPhonesRv = findViewById(R.id.getPhonesRv);
 
-        int nameFieldIndex = contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        searchBtn.setOnClickListener(btnSearchEvent);
+    }
+
+    View.OnClickListener btnSearchEvent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            drawPhoneItems(searchPhone(searchInp.getText().toString()));
+        }
+    };
+
+    public HashMap<String, String> getPhoneList() {
+        HashMap<String, String> phoneData = new HashMap<>();
+        ContentResolver cr = getContentResolver();
+        Cursor contacts = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER + " > 0", null, null);
+
+        int nameFieldIndex = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
         int numberFieldIndex = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
 
+        String name, number;
+
         while(contacts.moveToNext()) {
-            Log.d("G", "GETUsersPhones: " + contacts.getString(nameFieldIndex));
+            name = contacts.getString(nameFieldIndex);
+            number = contacts.getString(numberFieldIndex).replaceAll("-", "");
+            if(!phoneData.containsKey(number))
+                phoneData.put(number, name);
         }
+
+        contacts.close();
+
+        return phoneData;
+    }
+
+    public HashMap<String, String> searchPhone(String q) {
+        HashMap<String, String> result = new HashMap<>();
+        for(String key : phoneList.keySet()) {
+            String name = phoneList.get(key);
+            if(key.contains(q) || name.contains(q))
+                result.put(key, name);
+        }
+
+        return result;
+    }
+
+    public void drawPhoneItems(HashMap<String, String> items) {
+
     }
 }
